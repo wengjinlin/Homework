eea234b6111ca8576870387713466c5d9874fc1d
dandoujiang <154342226@qq.com>
2017-07-23 16:07:02 +0800

    Finish Login System

--- Day01/Login.py
+++ Day01/Login.py
@@ -2,55 +2,68 @@ while True:
     username = input("username:")
     # 在PyCharm中测试方便，采用明文密码
     password = input("password:")
+    #用户锁定状态指示器
+    user_locked = False
     #调用文件locked_user
-    print("调用文件locked_user")
-    if username == "locked":
-        print("用户已被锁定，请联系管理员！")
-        quit_sis = input("是否退出系统（y/n）？")
-        if quit_sis != "n":
+    fp_locked_user = open("locked_user", "r")
+    for line in fp_locked_user:
+        if username == line.strip():
+            user_locked = True
             break
-        else:
-            continue
+    fp_locked_user.close()
+    if user_locked:
+        print("用户已被锁定，请联系管理员！")
     else:
         #调用文件user
-        print("调用文件user")
-        #用户名验证成功
-        if username == "success" :
-            #密码验证成功
-            if password == "success":
+        fp_user = open("user","r")
+        #计数器，隔行验证用户名
+        count = 0
+        #用户名密码验证器
+        username_su = False
+        password_su = False
+        for line in fp_user:
+            if username_su:
+                #用户名验证成功的下一次循环，就是该用户的密码
+                password_in_file = line.strip()
+                if password == password_in_file:
+                    #验证密码成功
+                    password_su = True
+                #只验证一行，跳出循环
+                break
+            if count%2 == 0:
+                if username == line.strip():
+                    # 用户名验证成功
+                    username_su = True
+            count += 1
+        fp_user.close()
+        if username_su:
+            #用户名验证成功
+            if password_su:
+                # 密码验证成功
                 print("欢迎{_username}成功登陆系统".format(_username=username))
-                quit_sis = input("是否退出系统（y/n）？")
-                if quit_sis != "n":
-                    break
-                else:
-                    continue
-            #密码验证失败
             else:
+                # 密码验证失败
                 #最多还有2次输入机会
                 print("密码输入错误，你还有2次机会！")
                 for i in range(2):
                     password = input("password:")
-                    if password == "success":
+                    if password == password_in_file:
                         print("欢迎{_username}成功登陆系统".format(_username=username))
                         break
                     else:
                         if i == 0:
                             print("密码输入错误，你还有1次机会！")
                 else:
-                    #写入文件，锁定用户
                     print("密码连续输入错误已达3次，用户被锁定，请联系管理员！")
-                    username = "locked"
-                    print("写入文件，锁定用户")
-                quit_sis = input("是否退出系统（y/n）？")
-                if quit_sis != "n":
-                    break
-                else:
-                    continue
-        #用户名验证失败
+                    # 写入文件，锁定用户
+                    fp_locked_user = open("locked_user", "a")
+                    fp_locked_user.write(username+"\n")
+                    fp_locked_user.close()
         else:
+            # 用户名验证失败
             print("用户不存在！")
-            quit_sis = input("是否退出系统（y/n）？")
-            if quit_sis != "n":
-                break
-            else:
-                continue
+    quit_sis = input("是否退出系统（y/n）？")
+    if quit_sis != "n":
+        break
+    else:
+        continue
---
+++ Day01/test.md
@@ -0,0 +1,136 @@
+# Login程序的测试
+
+## 文件初始内容
+
+### 1.可登陆用户管理文件（user）
+```text
+admin
+123
+user1
+456
+user2
+789
+user3
+123
+user4
+123
+user5
+123
+user6
+123
+user7
+123
+```
+
+### 2.被锁定用户管理文件（locked_user）
+
+```text
+
+```
+
+## 开始测试
+
+### 1.用户admin成功登陆
+
+#### 	1).测试数据
+
+```
+admin 123
+```
+
+#### 	2).测试结果
+
+![](C:\Users\Administrator\Documents\Homework\Day01\test\test1.png)
+
+### 2.用户admin不被锁定
+
+#### 	1).测试数据
+
+```
+admin sdf 123
+admin sdf sdf 123
+```
+
+#### 	2).测试结果
+
+![](C:\Users\Administrator\Documents\Homework\Day01\test\test2.png)
+### 3.用户admin被锁定
+
+#### 	1).测试数据
+
+```
+admin sdf sdf sdf
+admin 123
+```
+
+#### 	2).测试结果
+
+![](C:\Users\Administrator\Documents\Homework\Day01\test\test3.png)
+### 4.用户user1输错1次密码(该密码存在文件中，但不是user1的密码)
+
+#### 	1).测试数据
+
+```
+user1 123
+user1 456
+```
+
+#### 	2).测试结果
+
+![](C:\Users\Administrator\Documents\Homework\Day01\test\test4.png)
+### 5.用户user1被锁定，再次登陆时无论密码对否直接显示用户被锁定
+
+#### 	1).测试数据
+
+```
+user1 sdf sdf sdf
+user1 sdf
+```
+
+
+#### 	2).测试结果
+
+![](C:\Users\Administrator\Documents\Homework\Day01\test\test5.png)
+### 6.非法用户登录，显示用户不存在
+
+#### 	1).测试数据
+
+```
+wer wer
+```
+
+
+#### 	2).测试结果
+
+![](C:\Users\Administrator\Documents\Homework\Day01\test\test6.png)
+
+## 程序结束后文件内容
+
+### 1.可登陆用户管理文件（user）
+```text
+admin
+123
+user1
+456
+user2
+789
+user3
+123
+user4
+123
+user5
+123
+user6
+123
+user7
+123
+```
+
+### 2.被锁定用户管理文件（locked_user）
+
+```text
+admin
+user1
+```
+
+
